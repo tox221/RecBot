c32e3a72aafbd9cff03a23a42db0a98f4708cb13
tox221 <168878601+tox221@users.noreply.github.com>
2025-03-21 03:36:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// إضافة وظيفة لإضافة البوت إلى السيرفر
-document.querySelector('.invite').addEventListener('click', function() {
-    window.location.href = 'https://discord.com/oauth2/authorize?client_id=1236082562368278659';
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('تم تحميل الموقع بنجاح!');
 });
