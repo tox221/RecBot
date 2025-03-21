7086e741849cee476c0180d7a130b75240c89d41
tox221 <168878601+tox221@users.noreply.github.com>
2025-03-21 03:25:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// إضافة وظيفة لإضافة البوت إلى السيرفر
+document.querySelector('.invite').addEventListener('click', function() {
+    window.location.href = 'https://discord.com/oauth2/authorize?client_id=1236082562368278659';
+});
